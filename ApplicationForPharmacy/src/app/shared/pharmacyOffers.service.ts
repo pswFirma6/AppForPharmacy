c70571c7dfa94d8809419412eba147797ba69008
milicaoui <vumilicaamm@gmail.com>
2021-11-14 10:48:38 +0100

    feat: service and model class for offers

---
+++ ApplicationForPharmacy/src/app/shared/pharmacyOffers.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { PharmacyOfferModel } from "./pharmacyOffer.model";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class PharmacyOffersService {
+
+    formCreatingNewOffer: PharmacyOfferModel = new PharmacyOfferModel();
+
+    constructor(private http: HttpClient, public service: PharmacyOffersService) { }
+
+    postLogin(){
+    }
+
+    createOffer(){
+    }
+  }
