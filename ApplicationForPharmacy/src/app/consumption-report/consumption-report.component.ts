100a9f76aa78b08b32ae8e9562f909f2304e9883
milicaoui <vumilicaamm@gmail.com>
2021-11-20 17:43:25 +0100

    feat: http request added

--- ApplicationForPharmacy/src/app/consumption-report/consumption-report.component.ts
+++ ApplicationForPharmacy/src/app/consumption-report/consumption-report.component.ts
@@ -14,7 +14,9 @@ export class ConsumptionReportComponent implements OnInit {
   }
 
   Download(){
-    this.service.downloadReport();
+
+    this.service.downloadReport()
+    .subscribe();
   }
 
 }
