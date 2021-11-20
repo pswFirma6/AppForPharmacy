0799db5ec43a94d7ca7cd30a53d44fc964712322
poparicsara <73502384+poparicsara@users.noreply.github.com>
2021-11-20 12:29:36 +0100

    feat: offers review

--- ApplicationForPharmacy/src/app/app.module.ts
+++ ApplicationForPharmacy/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MedicationsComponent } from './medications/medications.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LandingComponent } from './landing/landing.component';
 import { PharmacyOfferFormComponent } from './pharmacy-offer-form/pharmacy-offer-form.component';
+import { PharmacyOfferComponent } from './pharmacy-offer/pharmacy-offer.component';
+import { PharmacyOffersComponent } from './pharmacy-offers/pharmacy-offers.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { PharmacyOfferFormComponent } from './pharmacy-offer-form/pharmacy-offer
     MedicationsComponent,
     LandingComponent,
     PharmacyOfferFormComponent,
+    PharmacyOfferComponent,
+    PharmacyOffersComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,7 @@ import { PharmacyOfferFormComponent } from './pharmacy-offer-form/pharmacy-offer
       { path: 'hospitalRegistration', component: HospitalRegistrationComponent },
       { path: 'landing', component: LandingComponent },
       { path: 'medications', component: MedicationsComponent },
-      { path: 'offers', component: PharmacyOfferFormComponent },
+      { path: 'offers', component: PharmacyOfferComponent },
       { path: '', redirectTo: 'landing', pathMatch: 'full' },
       { path: '**', redirectTo: 'landing', pathMatch: 'full' }
     ]),
